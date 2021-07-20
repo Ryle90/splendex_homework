a9b10f3bc6ae80cf578aaa6cc213e21a7c693b03
Tábi István <tabi.pisti@gmail.com>
2021-07-20 18:39:39 +0200

    star components added

---
+++ splendex_homework/src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import Start from './components/Start';
+
+function App() {
+  return (
+    <div className="App">
+      <Start />
+    </div>
+  );
+}
+
+export default App;
---
+++ splendex_homework/src/components/Start.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export default function Start () {
+    const {possibleCardPairs, setPossibleCardPairs} = useState(0);
+
+    useEffect(() => {
+        let temp = [];
+        for (let i = 3; i <= 10; i++) {
+            temp.push(i)
+        };
+        setPossibleCardPairs(temp)
+    }, [])
+
+    return (
+        <div>
+            <h3>Splendex Memory Game</h3>
+            <div className="form-group">
+                <label htmlFor="deck-size"></label>
+                <select name="deck-size" id="deck-size">
+                    <option value="">Select number of card parts</option>
+                </select>
+                <button>Start new game</button>
+            </div>
+        </div>
+    )
+}
